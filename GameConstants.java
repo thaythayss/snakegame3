70c9396a748804b61b109b2a58f15527cf35da80
thaythayss <thays.castro.andrade@escola.pr.gov.br>
2026-09-16 09:10:07 -0300

    v2

---
+++ GameConstants.java
@@ -0,0 +1,15 @@
+public final class GameConstants {
+
+    private GameConstants() {
+
+        public static final String GAME_TITLE = "Snake Game";
+
+        public static final int TILE = 24; //formato de telha
+
+        public static final int COLS = 20; //colunas
+        public static final int ROWS = 20; //linhas
+
+        public static final let WIDTH = TITLE + COLS;
+        public static final int HEIGHT 
+    }
+}
